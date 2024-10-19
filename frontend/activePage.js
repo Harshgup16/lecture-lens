66216d21530ec52dd272f3b56913a7f48cc96973
Harshgub16 <hkgupta160420@gmail.com>
2024-10-19 13:22:39 +0530

    merge conflict

--- frontend/activePage.js
+++ frontend/activePage.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded", () => {
     const activePage = window.location.pathname; // Get the current page path
     const navLinks = document.querySelectorAll('#action_bar a'); // Select only nav links
@@ -10,16 +9,3 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
-=======
-document.addEventListener("DOMContentLoaded", () => {
-    const activePage = window.location.pathname; // Get the current page path
-    const navLinks = document.querySelectorAll('#action_bar a'); // Select only nav links
-
-    navLinks.forEach(link => {
-        // Check if the link's href includes the active page
-        if (link.href.includes(activePage)) {
-            link.classList.add('active'); // Add 'active' class to the current link
-        }
-    });
-});
->>>>>>> 698f0f17fb6c5a18403414d76267ca4ec99c7bcc
